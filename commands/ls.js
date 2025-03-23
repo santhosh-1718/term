f9f7bf6bde7aaf16741c7205e0dd921149bc384d
Santhosh J <147750600+santhosh-1718@users.noreply.github.com>
2025-03-23 16:09:08 +0530

    Create ls.js

---
+++ commands/ls.js
@@ -0,0 +1,9 @@
+// commands/ls.js
+export async function execute(args, currentDirHandle) {
+    if (!currentDirHandle) return 'No directory selected. Use `cd` to select a directory.';
+    const files = [];
+    for await (const entry of currentDirHandle.values()) {
+        files.push(entry.name);
+    }
+    return files.join('\n');
+}
